19243ddb38aa3bfb605210548ed19b30d5fa5635
diegowanborges <diegowanborges@gmail.com>
2021-07-22 15:01:47 -0300

    configuring dependencies

---
+++ backend/src/main/java/com/twokeys/twokeysweb/TwokeysWebApplication.java
@@ -0,0 +1,13 @@
+package com.twokeys.twokeysweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwokeysWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwokeysWebApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/twokeys/twokeysweb/TwokeysWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twokeys.twokeysweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwokeysWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
